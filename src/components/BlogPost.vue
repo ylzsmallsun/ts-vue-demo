4badc12ab0a39ecabdf71939114774b10a789ada
lingzhen <yanglingzhen531@pingan.com.cn>
2019-12-10 15:16:00 +0800

    feat: +ts demo page

---
+++ src/components/BlogPost.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="blogList">
+    <h2>{{ post.title }}</h2>
+		<p>{{ post.subTitle }}</p>
+		<p class="meta">Written by {{ post.author }} on {{ date }}</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+export interface IPost {
+  title: string;
+  subTitle: string;
+  author: string;
+  datePosted: Date;
+}
+
+@Component
+export default class HelloWorld extends Vue {
+  @Prop() private post!: IPost;
+  get date() {
+    return `${ this.post.datePosted.getDate() }/${ this.post.datePosted.getMonth() }/${ this.post.datePosted.getFullYear() }`;
+  }
+
+  private mounted(): void {
+    console.log('blog: ', this.post);
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h2 {
+  text-decoration: underline;
+}
+p.meta {
+  font-style: italic;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,51 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1 class="pageTitle">Typescript demo page</h1>
+    <BlogPost v-for="blog in blogPosts" :post="blog" :key="blog.title"/>
   </div>
 </template>
 
-<script>
+<script lang="ts">
+import { Component, Vue} from 'vue-property-decorator';
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import BlogPost, { IPost } from '@/components/BlogPost.vue';
 
-export default {
-  name: 'home',
+@Component({
   components: {
-    HelloWorld
+    BlogPost
+  }
+})
+
+export default class Home extends Vue {
+  private blogPosts: IPost[] = [
+    {
+      title: 'My first blogpost ever!',
+      subTitle: 'Lorem ipsum dolor sit amet.',
+      author: 'Mike',
+      datePosted: new Date(2018, 12, 10)
+    },
+    {
+      title: 'Look I am blogging!',
+      subTitle: 'Hurray for me, this is my second post.',
+      author: 'Mike',
+      datePosted: new Date(2019, 1, 16)
+    },
+    {
+      title: 'You don\'t known javascript!',
+      subTitle: 'Something you need to know.',
+      author: 'Mike',
+      datePosted: new Date(2019, 11, 22)
+    },
+  ];
+
+  private mounted(): void {
+    // console.log(this.blogPosts);
   }
 }
+
 </script>
+<style scoped>
+.pageTitle {
+  color: orangered;
+}
+</style>
